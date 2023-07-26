8fcb9815feedcead96211d7be4c7759e63b5b52b
sergio-usma <sergioandresusma@hotmail.com>
2023-07-26 15:29:05 -0500

    Fetch Rockets using rocketsSlice and reducers

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,38 +1,29 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rocketsSlice';
 import '../styles/Rockets.css';
 
 const Rockets = () => {
-  const [rockets, setRockets] = useState([]);
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
 
   useEffect(() => {
-    const fetchRockets = async () => {
-      const response = await fetch('https://api.spacexdata.com/v3/rockets');
-      const data = await response.json();
-      const rocketsMap = data.map((rocket) => ({
-        id: rocket.id,
-        rocket_name: rocket.rocket_name,
-        description: rocket.description,
-        flickr_images: rocket.flickr_images,
-      }));
-      setRockets(rocketsMap);
-    };
-    fetchRockets()
-      .catch((error) => error.message);
-  });
+    if (rockets.length === 0) {
+      dispatch(getRockets());
+    }
+  }, [dispatch, rockets.length]);
 
   return (
     <div className="rockets__container">
       {rockets.map((rocket) => (
         <div className="rockets__card" key={rocket.id}>
-          <img
-            src={rocket.flickr_images?.[0]}
-            width="180px"
-            alt={rocket.rocket_name}
-          />
+          <img src={rocket.flickr_images?.[0]} width="180px" alt={rocket.rocket_name} />
           <div>
             <h4>{rocket.rocket_name}</h4>
             <p>{rocket.description}</p>
-            <button type="button">Reserve Rocket</button>
+            <button className="rockets__reserveBtn" type="button">
+              Reserve Rocket
+            </button>
           </div>
         </div>
       ))}
