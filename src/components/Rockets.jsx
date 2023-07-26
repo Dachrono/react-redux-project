f02a02ec09b2ad5cee3f633de4a62ec60c55d94f
sergio-usma <sergioandresusma@hotmail.com>
2023-07-26 15:00:59 -0500

    Fetch Rockets using useState

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,13 +1,43 @@
+import { useState, useEffect } from 'react';
 import '../styles/Rockets.css';
 
-const Rockets = () => (
-  <div className="rockets__container">
-    <div className="rockets__card">
-      <h4>Rocket name</h4>
-      <p>Description</p>
-      <button type="button">Reserve Rocket</button>
+const Rockets = () => {
+  const [rockets, setRockets] = useState([]);
+
+  useEffect(() => {
+    const fetchRockets = async () => {
+      const response = await fetch('https://api.spacexdata.com/v3/rockets');
+      const data = await response.json();
+      const rocketsMap = data.map((rocket) => ({
+        id: rocket.id,
+        rocket_name: rocket.rocket_name,
+        description: rocket.description,
+        flickr_images: rocket.flickr_images,
+      }));
+      setRockets(rocketsMap);
+    };
+    fetchRockets()
+      .catch((error) => error.message);
+  });
+
+  return (
+    <div className="rockets__container">
+      {rockets.map((rocket) => (
+        <div className="rockets__card" key={rocket.id}>
+          <img
+            src={rocket.flickr_images?.[0]}
+            width="180px"
+            alt={rocket.rocket_name}
+          />
+          <div>
+            <h4>{rocket.rocket_name}</h4>
+            <p>{rocket.description}</p>
+            <button type="button">Reserve Rocket</button>
+          </div>
+        </div>
+      ))}
     </div>
-  </div>
-);
+  );
+};
 
 export default Rockets;
