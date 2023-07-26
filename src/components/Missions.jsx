ea4688314c5362170a113ce62dd26d92e2a218e7
Dachrono <andres_zamorano_@hotmail.com>
2023-07-26 11:40:38 -0600

    Create basic structure and little stayle for missions

---
+++ src/components/Missions.jsx
@@ -0,0 +1,22 @@
+import '../styles/Missions.css';
+
+const Missions = () => (
+  <>
+    <div className="test" />
+    <div className="titles">
+      <table>
+        <thead>
+          <tr>
+            <th>Missions</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        {}
+      </table>
+    </div>
+  </>
+);
+
+export default Missions;
