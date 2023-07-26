8f1e3648c0a51d63df24355f386d7560fa3e3c74
sergio-usma <sergioandresusma@hotmail.com>
2023-07-26 11:35:00 -0500

    Add Rockets component

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,11 +1,13 @@
-function Rockets() {
-  return (
-    <>
-      <div className="rockets">
-        rockets
-      </div>
-    </>
-  );
-}
+import '../styles/Rockets.css';
+
+const Rockets = () => (
+  <div className="rockets__container">
+    <div className="rockets__card">
+      <h4>Rocket name</h4>
+      <p>Description</p>
+      <button type="button">Reserve Rocket</button>
+    </div>
+  </div>
+);
 
 export default Rockets;
