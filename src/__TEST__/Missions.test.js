a099b1c6bb24085aa4fc1374ee64cec997414d68
Dachrono <andres_zamorano_@hotmail.com>
2023-07-27 19:17:03 -0600

    Create Missions tests

---
+++ src/__TEST__/Missions.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Missions from '../components/Missions'
+import '@testing-library/jest-dom'
+import store from '../redux/store'
+import { Provider } from 'react-redux';
+import { fetchdata } from '../redux/missionsSlice';
+
+describe ('Test missions component', () => {
+    test('Test render DOM', async () => {
+        await store.dispatch(fetchdata());
+        const missions = render ( <Provider store={store}><Missions /></Provider> )
+        expect(missions).toMatchSnapshot(); 
+    })
+
+    test('Test click button function', async () => {
+        await store.dispatch(fetchdata());
+        const missions = render ( <Provider store={store}><Missions /></Provider> )
+        const buttons = document.querySelectorAll('.graybtn');
+        fireEvent.click(buttons[0]);
+        const newBut = screen.getByText('Leave Mission');
+        expect(newBut).toBeInTheDocument();
+    })
+})
+
